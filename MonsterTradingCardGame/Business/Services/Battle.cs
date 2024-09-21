f98c54d01b268585c44f16caa1dd737d83955dd8
jakuslizy <jakuslizy@gmail.com>
2024-09-21 12:49:05 +0200

    Refactor: Battle Class

--- MonsterTradingCardGame/Business/Services/Battle.cs
+++ MonsterTradingCardGame/Business/Services/Battle.cs
@@ -2,15 +2,9 @@ using MonsterTradingCardGame.Domain.Models;
 
 namespace MonsterTradingCardGame.Business.Services;
 
-public class Battle
+public class Battle(User player1, User player2)
 {
-    private User Player1 { get; set; }
-    private User Player2 { get; set; }
+    private User Player1 { get; set; } = player1;
+    private User Player2 { get; set; } = player2;
     private const int MaxRounds = 100;
-    
-    public Battle(User player1, User player2)
-    {
-        Player1 = player1;
-        Player2 = player2;
-    }
 }
