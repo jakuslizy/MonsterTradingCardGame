a6fbd1ba5642d91b990906b455ff1feeec712e0f
Lizy <if23b211@technikum-wien.at>
2024-09-21 10:43:55 +0200

    Add Battle Class

---
+++ MonsterTradingCardGame/Battle.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MonsterTradingCardGame;
+
+public class Battle
+{
+    private User Player1 { get; set; }
+    private User Player2 { get; set; }
+    private const int MaxRounds = 100;
+    
+    public Battle(User player1, User player2)
+    {
+        Player1 = player1;
+        Player2 = player2;
+    }
+}
