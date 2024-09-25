352ece3bca9fb6fef9d8739163337c0ca2bf493a
Lizy <if23b211@technikum-wien.at>
2024-09-25 21:14:29 +0200

    feat: Improve damage calculation logic in MonsterCard class

--- MonsterTradingCardGame/Domain/Models/MonsterCard.cs
+++ MonsterTradingCardGame/Domain/Models/MonsterCard.cs
@@ -1,15 +1,16 @@
 namespace MonsterTradingCardGame.Domain.Models;
 
-public class MonsterCard(string name, int damage, ElementType elementType) : Card(name, damage, elementType)
+public class MonsterCard : Card
 {
+    public MonsterCard(string name, int damage, ElementType elementType) 
+        : base(name, damage, elementType)
+    {
+    }
+
     public override int CalculateDamage(Card opponent)
     {
-        // Wenn Gegner SpellCard ist, können Elementar-Effektivitäten berücksichtigt werden.
-        if (opponent is SpellCard)
-        {
-            return base.CalculateDamage(opponent);
-        }
-        // Bei Monster vs Monster: Elemente sind nicht wichtig
+        // Bei Monster vs Monster nur der Basisschaden wird berücksichtigt.
+        // Elemente sind nicht wichtig
         return Damage;
     }
 }
