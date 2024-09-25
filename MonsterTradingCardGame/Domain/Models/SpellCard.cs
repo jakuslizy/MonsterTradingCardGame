f72fe682547a51e45eddfe580ef5691df9a299dc
Lizy <if23b211@technikum-wien.at>
2024-09-25 21:15:14 +0200

    feat: Add elemental effectiveness logic to SpellCard class

--- MonsterTradingCardGame/Domain/Models/SpellCard.cs
+++ MonsterTradingCardGame/Domain/Models/SpellCard.cs
@@ -1,32 +1,40 @@
 namespace MonsterTradingCardGame.Domain.Models
 {
-    public class SpellCard(string name, int damage, ElementType elementType) : Card(name, damage, elementType)
+    public class SpellCard : Card
     {
+        public SpellCard(string name, int damage, ElementType elementType)
+            : base(name, damage, elementType)
+        {
+        }
+
         public override int CalculateDamage(Card opponent)
         {
+            //Basis Schaden berechnen und Element-Logik anwenden
             int baseDamage = base.CalculateDamage(opponent);
             return CalculateElementalDamage(opponent, baseDamage);
         }
-        
+
         private int CalculateElementalDamage(Card opponent, int baseDamage)
         {
             if (IsEffectiveAgainst(opponent.ElementType))
             {
                 return baseDamage * 2;
             }
-            else if (IsWeakAgainst(opponent.ElementType))
+            if (IsWeakAgainst(opponent.ElementType))
             {
                 return baseDamage / 2;
             }
+
             return baseDamage;
         }
-        
+
         private bool IsEffectiveAgainst(ElementType opponentElement)
         {
             return (ElementType == ElementType.Water && opponentElement == ElementType.Fire) ||
                    (ElementType == ElementType.Fire && opponentElement == ElementType.Normal) ||
                    (ElementType == ElementType.Normal && opponentElement == ElementType.Water);
         }
+
         private bool IsWeakAgainst(ElementType opponentElement)
         {
             return (ElementType == ElementType.Fire && opponentElement == ElementType.Water) ||
