8c0d97940c861762fc25057d0d9677e1f06e4f87
jakuslizy <jakuslizy@gmail.com>
2024-10-11 00:09:19 +0200

    Add LoginData

---
+++ MonsterTradingCardGame/Domain/Models/LoginData.cs
@@ -0,0 +1,7 @@
+namespace MonsterTradingCardGame.Domain.Models;
+
+public class LoginData
+{
+    public required string Username { get; init; }
+    public required string Password { get; init; }
+}
