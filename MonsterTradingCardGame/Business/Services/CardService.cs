fe5d70201a0490e7585ff3bb605abf4a11527135
Lizy <if23b211@technikum-wien.at>
2024-10-11 00:13:36 +0200

    Add CardService class

---
+++ MonsterTradingCardGame/Business/Services/CardService.cs
@@ -0,0 +1,65 @@
+using MonsterTradingCardGame.Data;
+using MonsterTradingCardGame.Domain.Models;
+
+namespace MonsterTradingCardGame.Business.Services;
+
+public class CardService
+{
+    public void CreatePackage(List<Card> cards)
+    {
+        var package = new Package();
+        foreach (var card in cards)
+        {
+            package.AddCard(card);
+        }
+
+        InMemoryDatabase.AddPackage(package);
+    }
+
+    public List<Card> AcquirePackage(User user)
+    {
+        if (user.Coins < Package.PackagePrice)
+        {
+            throw new InvalidOperationException("Not enough coins");
+        }
+
+        var package = InMemoryDatabase.GetPackage();
+        if (package == null)
+        {
+            throw new InvalidOperationException("No packages available");
+        }
+
+        user.UpdateCoins(user.Coins - Package.PackagePrice);
+        foreach (var card in package.GetCards())
+        {
+            user.AddCardToStack(card);
+        }
+
+        return package.GetCards().ToList();
+    }
+
+    public IReadOnlyList<Card> GetUserCards(User user)
+    {
+        return user.GetStack();
+    }
+
+    public void ConfigureDeck(User user, List<string> cardIds)
+    {
+        if (cardIds.Count != 4)
+        {
+            throw new InvalidOperationException("Deck must contain exactly 4 cards");
+        }
+
+        user.Deck.Clear();
+        foreach (var cardId in cardIds)
+        {
+            var card = user.GetStack().FirstOrDefault(c => c.Id == cardId);
+            if (card == null)
+            {
+                throw new InvalidOperationException($"Card with ID {cardId} not found in user's stack");
+            }
+
+            user.AddCardToDeck(card);
+        }
+    }
+}
