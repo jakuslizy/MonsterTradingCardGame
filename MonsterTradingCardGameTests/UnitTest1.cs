39a51ae9a239200fa00fd1753da2a709e68a87da
Lizy <if23b211@technikum-wien.at>
2024-10-22 22:17:36 +0200

    chore: translate test messages to English

--- MonsterTradingCardGameTests/UnitTest1.cs
+++ MonsterTradingCardGameTests/UnitTest1.cs
@@ -16,43 +16,35 @@ public class Tests
     [SetUp]
     public void Setup()
     {
-        // Repositories und Services initialisieren
         _userRepository = new UserRepository();
         _sessionRepository = new SessionRepository();
+        
         _userService = new UserService(_userRepository, _sessionRepository);
         _cardService = new CardService();
         _battleService = new BattleService();
         
-        // Router mit den Services
         _router = new Router(_userService, _cardService, _battleService);
     }
 
     [Test]
     public void TestHomeRoute()
     {
-        // Test der Home-Route
         var response = _router.RouteRequest("GET / HTTP/1.1", new Dictionary<string, string>(), "");
-        if (response != null)
-            Assert.Pass();
-        else
-            Assert.Fail();
+        Assert.IsNotNull(response, "The response should not be null.");
+        Assert.That(response.StatusCode, Is.EqualTo(200), "The Home route should return status code 200.");
     }
 
     [Test]
     public void TestUsersRoute()
     {
-        // Test der Users-Route
         var response = _router.RouteRequest("POST /users HTTP/1.1", new Dictionary<string, string>(), "");
-        if (response != null)
-            Assert.Pass();
-        else
-            Assert.Fail();
+        Assert.IsNotNull(response, "The response should not be null");
+        Assert.That(response.StatusCode, Is.EqualTo(400), "The Users route should return status code 400 (empty body)");
     }
 
     [Test]
     public void TestInvalidRoute()
     {
-        // Test einer ungültigen Route
         var response = _router.RouteRequest("GET /invalidpath HTTP/1.1", new Dictionary<string, string>(), "");
         Assert.That(response.StatusCode, Is.EqualTo(401));
     }
@@ -60,7 +52,6 @@ public class Tests
     [Test]
     public void TestNullRequest()
     {
-        // Test mit null als Request
         var response = _router.RouteRequest(null, new Dictionary<string, string>(), "");
         Assert.That(response.StatusCode, Is.EqualTo(400));
     }
