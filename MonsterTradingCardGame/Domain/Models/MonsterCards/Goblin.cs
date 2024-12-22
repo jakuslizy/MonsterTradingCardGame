9e4e610765daf7b84de594140d9a1089a0a611c6
Lizy <if23b211@technikum-wien.at>
2024-12-22 14:03:15 +0100

    feat(MonsterCards): Add name parameter to Ork and Goblin constructor

--- MonsterTradingCardGame/Domain/Models/MonsterCards/Goblin.cs
+++ MonsterTradingCardGame/Domain/Models/MonsterCards/Goblin.cs
@@ -2,8 +2,8 @@ namespace MonsterTradingCardGame.Domain.Models.MonsterCards;
 
 public class Goblin : MonsterCard
 {
-    public Goblin(string id, int damage, ElementType elementType)
-        : base(id, "Goblin", damage, elementType)
+    public Goblin(string id, string name, int damage, ElementType elementType)
+        : base(id, "Globlin", damage, elementType)
     {
     }
 }
--- MonsterTradingCardGame/Domain/Models/MonsterCards/Ork.cs
+++ MonsterTradingCardGame/Domain/Models/MonsterCards/Ork.cs
@@ -2,7 +2,7 @@ namespace MonsterTradingCardGame.Domain.Models.MonsterCards;
 
 public class Ork : MonsterCard
 {
-    public Ork(string id, int damage, ElementType elementType)
+    public Ork(string id, string name, int damage, ElementType elementType)
         : base(id, "Ork", damage, elementType)
     {
     }
